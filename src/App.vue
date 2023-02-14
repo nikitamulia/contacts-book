4efbc68fd703feb82c91fc0e149a0dccd1ffd11b
nikitamulia <crovone96@gmail.com>
2023-02-14 13:25:48 +0200

    add modal/sort by name/search by name/modal

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,96 @@
 <template>      
     <div class="app">
-        <post-form
+        <h1>Contacts page</h1>
+        <my-input
+        v-model="searchQuery"
+        placeholder="Search.."
+        />
+        <div class="app__btns">
+            <my-button
+            @click="showDialog"
+            >Create contact
+            </my-button>
+            <my-select
+            v-model="selectedSort"
+            :options="sortOptions"
+            />
+        </div>
+        <my-dialog v-model:show="dialogVisible">
+            <post-form
         @create="createPost"
         />
+        </my-dialog>
+        
         <post-list
-         :posts="posts"
+         :posts="sortedAndSearchedContact"
          @remove="removePost"
+         v-if="!isContactsLoading"
          />
+         <div v-else>Loading...</div>
     </div>
 </template>
 
 <script>
 import PostForm from "@/components/PostForm.vue";
 import PostList from "@/components/PostList.vue";
+import axios from "axios";
 export default{
     components: {
         PostList, PostForm
     },
     data() {
         return{
-            posts: [
-                {id: 1, title: 'javascript', body: 'Abou this'},
-                {id: 2, title: 'javascript2', body: 'Abou this3'},
-                {id: 3, title: 'javascript2', body: 'Abou this3'},
-            ],
-            
+            posts: [],
+            dialogVisible: false,
+            isContactsLoading: false,
+            selectedSort: '',
+            searchQuery: '',
+            sortOptions: [
+                {value: 'title', name: 'Sort by name'},
+                {value: 'body', name: 'Sort by number'},
+            ]
         }
     },
     methods: {
        createPost(post){
           this.posts.push(post);
+          this.dialogVisible = false;
         },
         removePost(post){
             this.posts = this.posts.filter(p => p.id !== post.id)
+        },
+        showDialog(){
+            this.dialogVisible = true;
+        },
+        async fetchContacts(){
+            try {
+                this.isContactsLoading = true;
+                const response = await axios.get('https://63e4e2f7c04baebbcdae55db.mockapi.io/api/v1/contacts');
+                this.posts = response.data;
+                console.log('response', response)
+            } catch (error) {
+                console.log('error', error)
+            } finally{
+                this.isContactsLoading = false;
+            }
         }
        
+    },
+    mounted(){
+        this.fetchContacts();
+    },
+    computed: {
+        sortedContacts() {
+            return [...this.posts].sort((a,b)=> {
+            return a[this.selectedSort]?.localeCompare(b[this.selectedSort])
+           })
+        },
+        sortedAndSearchedContact(){
+            return this.sortedContacts.filter(contact => contact.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
+        }
+    },
+    watch:{
+    
     }
 }
 </script>
@@ -49,7 +105,11 @@ export default{
     .app{
         padding: 20px;
     }
-
+    .app__btns{
+        margin: 15px 0;
+        display: flex;
+        justify-content: space-between;
+    }
     
 
    
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,15 +1,18 @@
 <template>
-    <div>
+    <div v-if="posts.length > 0">
        <h3> Posts list</h3>
-       <post-item 
-        v-for="post in posts"
-        :post="post"
-        :key="post.id"
-        @remove="$emit('remove', post)"
-        />
-        
+       <transition-group name="contact-list">
+            <post-item 
+            v-for="post in posts"
+            :post="post"
+            :key="post.id"
+            @remove="$emit('remove', post)"
+            />
+       </transition-group>
     </div>
-    
+    <h2 v-else>
+        No contacts
+    </h2>
            
   
 </template>
@@ -30,5 +33,20 @@ import PostItem from "@/components/PostItem.vue"
 </script>
 
 <style  scoped>
-    
+.contact-list{
+    display: inline-block;
+    margin-right: 10px;
+}
+.contact-list-enter-active,
+.contact-list-leave-active {
+  transition: all 0.5s ease;
+}
+.contact-list-enter-from,
+.contact-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+.contact-list-move{
+    transition: transform 0.5s ease;
+}
 </style>
